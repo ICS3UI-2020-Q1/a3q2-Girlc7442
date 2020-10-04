0e1fcfa1ae3f2a88ad333a05076331bff3f84222
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 12:16:09 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * Determines what an animal is using a dichotomous key
+ * @author Cole Girling
  */
 public class Main {
 
@@ -9,7 +11,40 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // input scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //ask the user if the animal has feathers
+    System.out.println("Does the animal have feathers?");
+    String featherAnswer = input.nextLine();
     
+    //if the user answers yes to feathers
+    if(featherAnswer.equals ("yes")){
+      //ask if the animal swims'
+      System.out.println("Does the animal swim?");
+      String swimAnswer = input.nextLine();
+
+      //if the user answered yes to the animal swimming
+      if(swimAnswer.equals ("yes")){
+        System.out.println("This is a duck");
+      }else{
+        //if the user answered no to the animal swimming
+        System.out.println("This is a hen");
+      }
+
+    //if the user answered no to feathers
+    }else{
+      //ask the user if the animal has legs
+      System.out.println("Does the animal have legs?");
+      String legAnswer = input.nextLine();
+
+      //if the user answered yes to legs
+      if(legAnswer.equals ("yes")){
+        System.out.println("This is a lizard");
+      }else{
+        //if the user answered no to legs
+        System.out.println("This is a snake");
+      }
+    }
   }
 }
